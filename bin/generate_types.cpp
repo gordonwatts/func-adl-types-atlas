b8e58194a53fc979d4fc78ad8d80a5ac38d78ec1
Gordon Watts <gordon@gordonandpaula.net>
2021-10-12 00:20:16 +0200

    Correctly suppress non-useful classes
    This was pretty brutal and not useful for anything but most basic.

--- bin/generate_types.cpp
+++ bin/generate_types.cpp
@@ -19,6 +19,8 @@
 #include <iostream>
 #include <queue>
 #include <set>
+#include <algorithm>
+#include <iterator>
 
 using namespace std;
 
@@ -33,7 +35,38 @@ bool can_emit_class(const class_info &c_info) {
     if (c_info.name_as_type.template_arguments.size() > 0) {
         return false;
     }
+    if (c_info.methods.size() == 0) {
+        return false;
+    }
     return true;
+}
+
+// Return true if this method can actually be emitted.
+bool can_emit_method(const method_info &meth, const set<string> &classes_to_emit) {
+    // Make sure returns something.
+    if (meth.return_type.size() == 0) {
+        return false;
+    }
+
+    // Get all referenced methods, and make sure we know about those objects.
+    // Or we can't support this guy.
+    auto method_types = referenced_types(meth);
+    set<string> method_types_set(method_types.begin(), method_types.end());
+    if (!includes(classes_to_emit.begin(), classes_to_emit.end(), method_types_set.begin(), method_types_set.end())) {
+        return false;
+    }
+    return true;
+}
+
+// Return true if any method can be emitted
+bool can_emit_any_methods(const vector<method_info> &methods, const set<string> &classes_to_emit) {
+    for (auto &&m : methods)
+    {
+        if (can_emit_method(m, classes_to_emit)) {
+            return true;
+        }
+    }
+    return false;
 }
 
 int main(int, char**) {
@@ -90,7 +123,7 @@ int main(int, char**) {
         }
     }
 
-    // Look at the loaded type defs, and add alaises.
+    // Look at the loaded type defs, and add aliases.
     fixup_type_aliases(done_classes);
 
     // Get the list of containers from the classes. These will be top level collections
@@ -155,6 +188,32 @@ int main(int, char**) {
     classes_to_emit.insert("int");
     classes_to_emit.insert("float");
 
+    // Now, we need to loop through all of these things until we get a stable set of classes that we can emit.
+    // Yes, this is painful.
+    bool modified = true;
+    while (modified) {
+        modified = false;
+        set<string> bad_classes;
+        for (auto &&c_name : classes_to_emit)
+        {
+            auto class_info_ptr = class_map.find(c_name);
+            if (class_info_ptr != class_map.end()) {
+                auto &&class_info = class_info_ptr->second;
+                if (!can_emit_any_methods(class_info.methods, classes_to_emit)) {
+                    bad_classes.insert(c_name);
+                }
+            }
+        }
+        if (bad_classes.size() > 0) {
+            modified = true;
+            for (auto &&b_c : bad_classes)
+            {
+                classes_to_emit.erase(b_c);
+            }
+        }        
+    }
+
+    // Finally, we actually emit these.
     out << YAML::Key << "classes"
         << YAML::Value
         << YAML::BeginSeq;
@@ -170,6 +229,11 @@ int main(int, char**) {
             continue;
         }
 
+        // Make sure there is at least one method
+        if (!can_emit_any_methods(c_info->second.methods, classes_to_emit)) {
+            continue;
+        }
+
         // If we can dump the class, then we should!
         out << YAML::BeginMap
             << YAML::Key << "name" << YAML::Value << normalized_type_name(c_info->second.name_as_type);
@@ -182,50 +246,37 @@ int main(int, char**) {
         bool first_method = true;
         for (auto &&meth : c_info->second.methods)
         {
-            // Make sure returns something.
-            if (meth.return_type.size() == 0) {
-                continue;
-            }
-
-            // Get all referenced methods, and make sure we know about those objects.
-            // Or we can't support this guy.
-            auto method_types = referenced_types(meth);
-            set<string> method_types_set(method_types.begin(), method_types.end());
-            if (!includes(classes_to_emit.begin(), classes_to_emit.end(), method_types_set.begin(), method_types_set.end())) {
-                continue;
-            }
-
-            if (first_method) {
-                out << YAML::Key << "methods"
-                    << YAML::Value
-                    << YAML::BeginSeq;
-                first_method = false;
-            }
-
-            out << YAML::BeginMap
-                << YAML::Key << "name" << YAML::Value << meth.name
-                << YAML::Key << "return_type" << YAML::Value << normalized_type_name(meth.return_type);
-
-            bool first_argument = true;
-            for (auto &&arg : meth.arguments)
-            {
-                if (first_argument) {
-                    first_argument = false;
-                    out << YAML::Key << "arguments"
+            if (can_emit_method(meth, classes_to_emit)) {
+                if (first_method) {
+                    out << YAML::Key << "methods"
                         << YAML::Value
                         << YAML::BeginSeq;
+                    first_method = false;
                 }
-                out << YAML::BeginMap
-                    << YAML::Key << "name" << YAML::Value << arg.name
-                    << YAML::Key << "type" << YAML::Value << normalized_type_name(arg.full_typename)
-                    << YAML::EndMap;
-            }
-            if (!first_argument) {
-                out << YAML::EndSeq;
-            }
-            
 
-            out << YAML::EndMap;
+                out << YAML::BeginMap
+                    << YAML::Key << "name" << YAML::Value << meth.name
+                    << YAML::Key << "return_type" << YAML::Value << normalized_type_name(meth.return_type);
+
+                bool first_argument = true;
+                for (auto &&arg : meth.arguments)
+                {
+                    if (first_argument) {
+                        first_argument = false;
+                        out << YAML::Key << "arguments"
+                            << YAML::Value
+                            << YAML::BeginSeq;
+                    }
+                    out << YAML::BeginMap
+                        << YAML::Key << "name" << YAML::Value << arg.name
+                        << YAML::Key << "type" << YAML::Value << normalized_type_name(arg.full_typename)
+                        << YAML::EndMap;
+                }
+                if (!first_argument) {
+                    out << YAML::EndSeq;
+                }
+                out << YAML::EndMap;
+            }
         }
 
         if (!first_method) {
