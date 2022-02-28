cf7eab1304812f799ce990345d1fd969e050e5d3
Gordon Watts <gordon@gordonandpaula.net>
2022-02-28 02:54:28 +0100

    Added overlap removal

--- metadata/r21/common_corrections.py
+++ metadata/r21/common_corrections.py
@@ -6,7 +6,6 @@ jetSequence.JvtEfficiencyAlg.truthJetCollection = '{{calib.jet_calib_truth_colle
 jetSequence.ForwardJvtEfficiencyAlg.truthJetCollection = '{{calib.jet_calib_truth_collection}}'
 calibrationAlgSeq += jetSequence
 print( jetSequence ) # For debugging
-#
 # Include, and then set up the jet analysis algorithm sequence:
 from JetAnalysisAlgorithms.JetJvtAnalysisSequence import makeJetJvtAnalysisSequence
 jvtSequence = makeJetJvtAnalysisSequence( 'mc', jetContainer, enableCutflow=True )
@@ -15,7 +14,6 @@ jvtSequence.configure( inputName = {'jets'      : jetContainer + '_Base_%SYS%' }
                        )
 calibrationAlgSeq += jvtSequence
 print( jvtSequence ) # For debugging
-# Output jet_collection = {{calib.jet_collection}}Calib_NOSYS
 #
 muon_container = '{{calib.muon_collection}}'
 from MuonAnalysisAlgorithms.MuonAnalysisSequence import makeMuonAnalysisSequence
@@ -24,15 +22,13 @@ muonSequence.configure( inputName = muon_container,
                         outputName = muon_container + 'Calib_{{calib.muon_working_point}}{{calib.muon_isolation}}_%SYS%' )
 calibrationAlgSeq += muonSequence
 print( muonSequence ) # For debugging
-# Output muon_collection = {{calib.jet_collection}}Calib_{{calib.muon_working_point}}{{calib.muon_isolation}}_NOSYS
 #
 from EgammaAnalysisAlgorithms.ElectronAnalysisSequence import makeElectronAnalysisSequence
 electronSequence = makeElectronAnalysisSequence( 'mc', '{{calib.electron_working_point}}.{{calib.electron_isolation}}', postfix = '{{calib.electron_working_point}}_{{calib.electron_isolation}}')
 electronSequence.configure( inputName = '{{calib.electron_collection}}',
-                            outputName = '{{calib.electron_collection}}_{{working_point}}_{{isolation}}_%SYS%' )
+                            outputName = '{{calib.electron_collection}}_{{calib.working_point}}_{{calib.isolation}}_%SYS%' )
 calibrationAlgSeq += electronSequence
 print( electronSequence ) # For debugging
-# Output electron_collection = {{calib.electron_collection}}_{{working_point}}_{{isolation}}_NOSYS
 #TODO: Get photon corrections working. It does not seem possible in R21 and on PHYS_DAOD.
 # If you remove the fudge tool, then the photon eff tool has trouble.
 # If you remove that, then the sequence has trouble. Needs more work.
@@ -47,11 +43,34 @@ print( electronSequence ) # For debugging
 # del photonSequence.PhotonIsolationCorrectionAlg_Tight_FixedCutTight
 # calibrationAlgSeq += photonSequence
 # TODO: THis needs to be fixed as well!!
-# Output photon_collection = {{calib.jet_collection}}
 from TauAnalysisAlgorithms.TauAnalysisSequence import makeTauAnalysisSequence
 tauSequence = makeTauAnalysisSequence( 'mc', '{{calib.tau_working_point}}', postfix = '{{calib.tau_working_point}}', rerunTruthMatching=False)
 tauSequence.configure( inputName = '{{calib.tau_collection}}',
                        outputName = '{{calib.tau_collection}}_{{calib.tau_working_point}}_%SYS%' )
 calibrationAlgSeq += tauSequence
 print( tauSequence ) # For debugging
-# Output tau_collection = {{calib.tau_collection}}_{{calib.tau_working_point}}_NOSYS
+
+# Overlap removal
+from AsgAnalysisAlgorithms.OverlapAnalysisSequence import makeOverlapAnalysisSequence
+overlapSequence = makeOverlapAnalysisSequence('mc')
+overlapSequence.configure(
+    inputName = {
+        'electrons' : '{{calib.electron_collection}}_{{calib.working_point}}_{{calib.isolation}}_%SYS%',
+        'photons'   : '{{calib.photon_collection}}',
+        'muons'     : '{{calib.muon_collection}}Calib_{{calib.muon_working_point}}{{calib.muon_isolation}}_%SYS%',
+        'jets'      : '{{calib.jet_collection}}Calib_%SYS%',
+        'taus'      : '{{calib.tau_collection}}_{{calib.tau_working_point}}_%SYS%'
+    },
+    outputName = {
+        'electrons' : '{{calib.electron_collection}}_{{calib.working_point}}_{{calib.isolation}}_OR_%SYS%',
+        'photons'   : '{{calib.photon_collection}}_OR',
+        'muons'     : '{{calib.muon_collection}}Calib_{{calib.muon_working_point}}{{calib.muon_isolation}}_OR_%SYS%',
+        'jets'      : '{{calib.jet_collection}}Calib_OR_%SYS%',
+        'taus'      : '{{calib.tau_collection}}_{{calib.tau_working_point}}_OR_%SYS%'
+    })
+calibrationAlgSeq += overlapSequence
+# Output electron_collection = {{calib.electron_collection}}_{{working_point}}_{{isolation}}_OR_NOSYS
+# Output photon_collection = {{calib.photon_collection}}_OR
+# Output muon_collection = {{calib.muon_collection}}Calib_{{calib.muon_working_point}}{{calib.muon_isolation}}_OR_NOSYS
+# Output jet_collection = {{calib.jet_collection}}Calib_OR_NOSYS
+# Output tau_collection = {{calib.tau_collection}}_{{calib.tau_working_point}}_OR_NOSYS
