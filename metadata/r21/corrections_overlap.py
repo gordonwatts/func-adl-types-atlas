8e6a31c116510b089b59d1e11e8818beb541f25c
Gordon Watts <gordon@gordonandpaula.net>
2022-03-12 14:17:12 +0100

    Dropped bank name propagation fixed

--- metadata/r21/corrections_overlap.py
+++ metadata/r21/corrections_overlap.py
@@ -2,21 +2,21 @@ from AsgAnalysisAlgorithms.OverlapAnalysisSequence import makeOverlapAnalysisSeq
 overlapSequence = makeOverlapAnalysisSequence('mc')
 overlapSequence.configure(
     inputName = {
-        'electrons' : '{{calib.electron_collection}}_{{calib.working_point}}_{{calib.isolation}}_%SYS%',
+        'electrons' : output_electron_container,
         'photons'   : '{{calib.photon_collection}}',
-        'muons'     : '{{calib.muon_collection}}Calib_{{calib.muon_working_point}}{{calib.muon_isolation}}_%SYS%',
-        'jets'      : '{{calib.jet_collection}}Calib_%SYS%',
-        'taus'      : '{{calib.tau_collection}}_{{calib.tau_working_point}}_%SYS%'
+        'muons'     : output_muon_container,
+        'jets'      : output_jet_container,
+        'taus'      : output_tau_container,
     },
     outputName = {
-        'electrons' : '{{calib.electron_collection}}_{{calib.working_point}}_{{calib.isolation}}_OR_%SYS%',
+        'electrons' : '{{calib.electron_collection}}_{{calib.electron_working_point}}_{{calib.electron_isolation}}_OR_%SYS%',
         'photons'   : '{{calib.photon_collection}}_OR_%SYS%',
         'muons'     : '{{calib.muon_collection}}Calib_{{calib.muon_working_point}}{{calib.muon_isolation}}_OR_%SYS%',
         'jets'      : '{{calib.jet_collection}}Calib_OR_%SYS%',
         'taus'      : '{{calib.tau_collection}}_{{calib.tau_working_point}}_OR_%SYS%'
     })
 calibrationAlgSeq += overlapSequence
-# Output electron_collection = {{calib.electron_collection}}_{{working_point}}_{{isolation}}_OR_{{ sys_error }}
+# Output electron_collection = {{calib.electron_collection}}_{{calib.electron_working_point}}_{{calib.electron_isolation}}_OR_{{ sys_error }}
 # Output photon_collection = {{calib.photon_collection}}_OR_{{ ssy_error }}
 # Output muon_collection = {{calib.muon_collection}}Calib_{{calib.muon_working_point}}{{calib.muon_isolation}}_OR_{{ sys_error }}
 # Output jet_collection = {{calib.jet_collection}}Calib_OR_{{ sys_error }}
--- metadata/r21/corrections_tau.py
+++ metadata/r21/corrections_tau.py
@@ -4,4 +4,5 @@ tauSequence.configure( inputName = '{{calib.tau_collection}}',
                        outputName = '{{calib.tau_collection}}_{{calib.tau_working_point}}_%SYS%' )
 calibrationAlgSeq += tauSequence
 print( tauSequence ) # For debugging
+output_tau_container = '{{calib.tau_collection}}_{{calib.tau_working_point}}_%SYS%'
 # Output tau_collection = {{calib.tau_collection}}_{{calib.tau_working_point}}_{{ sys_error }}
