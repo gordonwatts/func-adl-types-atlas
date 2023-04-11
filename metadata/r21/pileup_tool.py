23a8a04f663ac5487cfef47742ced91c685394b5
Gordon Watts <gwatts@uw.edu>
2023-04-11 18:03:08 +0200

    Enable correct parsing of the pileup period

--- metadata/r21/pileup_tool.py
+++ metadata/r21/pileup_tool.py
@@ -1,5 +1,12 @@
 from AsgAnalysisAlgorithms.PileupAnalysisSequence import makePileupAnalysisSequence
-pileupSequence = makePileupAnalysisSequence( 'mc' )
-pileupSequence.configure( inputName = {}, outputName = {} )
-print( pileupSequence ) # For debugging
+
+# Use the sh object (sample Handler) to get the first tile and extract the filename
+# from it, which can then be used to fetch the MC campaign. `calib.datatype`
+# should contain `data` or `mc`
+pileupSequence = makePileupAnalysisSequence(
+    "{{calib.datatype}}", files=sh.at(0).fileName(0)
+)
+pileupSequence.configure(inputName={}, outputName={})
+print(pileupSequence)  # For debugging
+
 calibrationAlgSeq += pileupSequence
