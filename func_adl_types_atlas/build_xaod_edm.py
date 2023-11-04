1f263d4e0e971e27680396d384b5af040b4d01cb
Gordon Watts <gwatts@uw.edu>
2023-11-03 18:23:11 -0700

    Check permissions on the host

--- func_adl_types_atlas/build_xaod_edm.py
+++ func_adl_types_atlas/build_xaod_edm.py
@@ -19,6 +19,7 @@ def build_xaod_edm(release: str, output_file: Path):
     # 2. the path to the output file inside the container
     # These are used in the `docker run` command below.
     package_path_binding = f"{package_path}:/workdir/func_adl_xaod_types:rw"
+    os.chmod(output_dir, 0o777)
     output_dir_binding = f"{output_dir}:/workdir/output:rw"
 
     # And the image name to build the container
