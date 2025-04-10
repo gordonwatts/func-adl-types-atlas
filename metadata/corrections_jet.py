a46af25ffe1550e276a2076c8662e9562d1bf6bb
Roger Janusiak <roger22011@gmail.com>
2025-04-10 09:24:31 -0700

    Put the jet corrections in the new format

--- metadata/corrections_jet.py
+++ metadata/corrections_jet.py
@@ -1,34 +1,16 @@
 jetContainer = "{{calib.jet_collection}}"
-from JetAnalysisAlgorithms.JetAnalysisSequence import makeJetAnalysisSequence
-
-# Do not run ghost muon association if you have already
-# corrected objects.
-jetSequence = makeJetAnalysisSequence(
-    "{{calib.datatype}}",
-    jetContainer,
-    runGhostMuonAssociation={{calib.run_jet_ghost_muon_association}},
-)
-jetSequence.configure(inputName=jetContainer, outputName=jetContainer + "_Base_%SYS%")
-jetSequence.JvtEfficiencyAlg.truthJetCollection = "{{calib.jet_calib_truth_collection}}"
-try:
-    jetSequence.ForwardJvtEfficiencyAlg.truthJetCollection = (
-        "{{calib.jet_calib_truth_collection}}"
-    )
-except AttributeError:
-    pass
-
-calibrationAlgSeq += jetSequence
-print(jetSequence)  # For debugging
-
-# Include, and then set up the jet analysis algorithm sequence:
-from JetAnalysisAlgorithms.JetJvtAnalysisSequence import makeJetJvtAnalysisSequence
-
-jvtSequence = makeJetJvtAnalysisSequence("mc", jetContainer, enableCutflow=True)
-jvtSequence.configure(
-    inputName={"jets": jetContainer + "_Base_%SYS%"},
-    outputName={"jets": jetContainer + "Calib_%SYS%"},
-)
-calibrationAlgSeq += jvtSequence
-print(jvtSequence)  # For debugging
 output_jet_container = "{{calib.jet_collection}}Calib_%SYS%"
+
+config.addBlock('Jets')
+config.setOptions (containerName=output_jet_container)
+config.setOptions (jetCollection=jetContainer)
+config.setOptions (runJvtUpdate=False)
+config.setOptions (runNNJvtUpdate=True)
+config.setOptions (recalibratePhyslite=False)
+
+# TODO: Figure out why this needs to be here:
 # Output jet_collection = {{calib.jet_collection}}Calib_{{ sys_error }}
+
+# TODO: Figure out if these need to stay or not:
+#test2 = "{{calib.run_jet_ghost_muon_association}}"
+#test3 = "{{calib.jet_calib_truth_collection}}"
