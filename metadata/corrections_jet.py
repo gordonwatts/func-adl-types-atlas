d9a5d0a0f44b012461ddd30e878e8ca47f800639
Roger Janusiak <roger22011@gmail.com>
2025-04-24 08:44:45 -0700

    Jet corrections now works

--- metadata/corrections_jet.py
+++ metadata/corrections_jet.py
@@ -1,27 +1,15 @@
-jetContainer = "{{calib.jet_collection}}"
-output_jet_container = "{{calib.jet_collection}}Calib_%SYS%"
-
 config.addBlock('Jets')
-config.setOptions (containerName=output_jet_container)
-config.setOptions (jetCollection=jetContainer)
+config.setOptions (containerName="AnaJets")
+config.setOptions (jetCollection="{{calib.jet_collection}}")
 config.setOptions (runJvtUpdate=False)
-config.setOptions (runNNJvtUpdate=True)
+config.setOptions (runNNJvtUpdate=False)
 config.setOptions (recalibratePhyslite=False)
 
+config.addBlock ('Thinning')
+config.setOptions (containerName="{{calib.jet_calib_truth_collection}}")
+config.setOptions (outputName='OutTruthJets')
+config.setOptions (skipOnData=True)
 
-config.addBlock('Jets.JVT', containerName=output_jet_container)
+# Output jet_collection = {{calib.jet_collection}}
 
-config.addBlock ('SystObjectLink')
-config.setOptions (containerName='AnaJets')
-
-config.addBlock ('ObjectCutFlow')
-config.setOptions (containerName='AnaJets')
-config.setOptions (selectionName='jvt')
-
-
-# TODO: Figure out why this needs to be here:
-# Output jet_collection = {{calib.jet_collection}}Calib_{{ sys_error }}
-
-# TODO: Figure out if these need to stay or not:
-#test2 = "{{calib.run_jet_ghost_muon_association}}"
-#test3 = "{{calib.jet_calib_truth_collection}}"
+# TODO: Add ghost muon association: "{{calib.run_jet_ghost_muon_association}}"
--- metadata/sys_error_tool.py
+++ metadata/sys_error_tool.py
@@ -1,24 +1,10 @@
-from AnaAlgorithm.AlgSequence import AlgSequence
-from AnalysisAlgorithmsConfig.ConfigAccumulator import ConfigAccumulator
 from AnalysisAlgorithmsConfig.ConfigText import TextConfig
-from Campaigns.Utils import Campaign
-from AthenaConfiguration.AllConfigFlags import initConfigFlags
-
-dataType='mc'
-isPhyslite=False
-geometry='RUN2'
-campaign=Campaign.MC20e
-
-# Needed to configure the AlgSequence
-from AthenaConfiguration.AllConfigFlags import initConfigFlags
-flags = initConfigFlags()
-flags.Input.Files = [sh.at(0).fileName(0)]
-flags.lock()
-
-autoconfigFromFlags=flags
 
 config = TextConfig()
 
 # Switch on systematics
 config.addBlock('CommonServices')
-config.setOptions(systematicsHistogram='systematicsList')
+config.setOptions (runSystematics=True)
+
+import logging
+logging.basicConfig(level=logging.INFO)
