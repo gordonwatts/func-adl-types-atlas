6cd44744cb76285bdac23514b8dfd28918cb0fef
RogerJanusiak <roger22011@gmail.com>
2025-05-22 18:43:03 -0500

    Fixed runGhostMuonAssociation

--- metadata/25/corrections_jet.py
+++ metadata/25/corrections_jet.py
@@ -6,6 +6,6 @@ config.setOptions (jetCollection='{{calib.jet_collection}}')
 config.setOptions (runJvtUpdate=False)
 config.setOptions (runNNJvtUpdate=False)
 config.setOptions (recalibratePhyslite=False)
-config.setOptions (runGhostMuonAssociation='{{calib.run_jet_ghost_muon_association}}')
+config.setOptions (runGhostMuonAssociation={{calib.run_jet_ghost_muon_association}})
 
 # Output jet_collection = {{calib.jet_collection}}_Calib_{{ sys_error }}
