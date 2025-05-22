af5805c54f725913f618ae3cc63f6734462ae082
RogerJanusiak <roger22011@gmail.com>
2025-05-22 18:10:41 -0500

    Updated formating to be consistent.

--- metadata/25/corrections_jet.py
+++ metadata/25/corrections_jet.py
@@ -1,11 +1,11 @@
+jet_container = '{{calib.jet_collection}}_Calib'
+
 config.addBlock('Jets')
-config.setOptions (containerName="{{calib.jet_collection}}_Calib")
-config.setOptions (jetCollection="{{calib.jet_collection}}")
+config.setOptions (containerName=jet_container)
+config.setOptions (jetCollection='{{calib.jet_collection}}')
 config.setOptions (runJvtUpdate=False)
 config.setOptions (runNNJvtUpdate=False)
 config.setOptions (recalibratePhyslite=False)
-config.setOptions (runGhostMuonAssociation={{calib.run_jet_ghost_muon_association}})
+config.setOptions (runGhostMuonAssociation='{{calib.run_jet_ghost_muon_association}}')
 
 # Output jet_collection = {{calib.jet_collection}}_Calib_{{ sys_error }}
-
-# TODO: The jets calibrations are running even when we are not running on jets.
